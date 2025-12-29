de1b5667aa52263328e9b066f220af8dd44a7c22
Craig <craigf-svg@users.noreply.github.com>
2025-12-29 16:05:51 -0500

    add map commands

--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import (
 	"strings"
 	"bufio"
 	"os"
+	"net/http"
+	"io"
+	"log"
+	"encoding/json"
 )
+
+type config struct {
+    next     *string
+    previous *string
+}
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
+
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -23,11 +33,23 @@ func main() {
         callback:    commandExit,
     },
 		"help": {
-				name:				 "help",
-				description: "Displays a help message",
-				callback:		 commandHelp,
+			  name:        "help",
+		    description: "Displays a help message",
+			  callback:    commandHelp,
+		}, 
+		"map": {
+			  name:        "map",
+        description: "Print next map of the area",
+        callback:    commandMap,
+		},
+		"mapb": {
+		    name:        "mapb",
+			  description: "Print previous map of the area",
+			  callback:    commandMapB,
 		},
 	}
+
+	cfg := &config{}
 
 	for {
 		fmt.Printf("Pokedex > ")
@@ -41,7 +63,7 @@ func main() {
 		command := clean[0]
 		
 		if cli, ok := cliCommands[command]; ok {
-			cli.callback()
+			cli.callback(cfg)
 		} else {
 			fmt.Println("Unknown command")
 		}
@@ -50,21 +72,136 @@ func main() {
 	return
 }
 
-func commandExit() error {
+func commandExit(cfg *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")	
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(cfg *config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:\n")
-
+	fmt.Println("Map: Print the current map page")
+	fmt.Println("Mapb: Print the previous map page")
 	fmt.Println("help: Displays a help message")
 	fmt.Println("exit: Exit the Pokedex")
 	/* for _, message := range cliCommands {
 		fmt.Println(message.description)
 	} */
+	return nil
+}
+
+type locationResult struct {
+    Count    int       `json:"count"`
+    Next     *string   `json:"next"`
+    Previous *string   `json:"previous"`
+    Results  []struct {
+        Name string `json:"name"`
+        URL  string `json:"url"`
+    } `json:"results"`
+}
+
+func commandMap(cfg *config) error {
+	fmt.Println("Map")
+	getURL := "https://pokeapi.co/api/v2/location-area"
+	if cfg.next != nil && *cfg.next != "" {
+			getURL = *cfg.next
+	}
+
+	res, err := http.Get(getURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	locations := locationResult{}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	unmarshalErr := json.Unmarshal(body, &locations)
+	if unmarshalErr != nil {
+		fmt.Println(unmarshalErr)
+	}
+
+	cfg.next = locations.Next
+	cfg.previous = locations.Previous
+
+	// DEBUG PRINT NEXT
+	if cfg.next == nil {
+		fmt.Println("No next, you're on the last page")
+	} else {
+		fmt.Println(*cfg.next)
+	}
+	// DEBUG PRINT PREVIOUS
+	if cfg.previous == nil {
+		fmt.Println("No previous, you're on the first page")
+	} else {
+		fmt.Println(*cfg.previous)
+	}
+
+	for _, loc := range locations.Results {
+    fmt.Println(loc.Name)
+	}
+	return nil
+}
+
+func commandMapB(cfg *config) error {
+	fmt.Println("MapB")
+	if cfg.previous == nil {
+    fmt.Println("you're on the first page")
+    return nil
+	}
+	getURL := ""
+	if cfg.previous != nil && *cfg.previous != "" {
+			getURL = *cfg.previous
+	} else {
+		fmt.Println("Previous url could not be found")
+	}
+
+	res, err := http.Get(getURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	locations := locationResult{}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	unmarshalErr := json.Unmarshal(body, &locations)
+	if unmarshalErr != nil {
+		fmt.Println(unmarshalErr)
+	}
+
+	cfg.next = locations.Next
+	cfg.previous = locations.Previous
+	// DEBUG PRINT NEXT
+	if cfg.next == nil {
+		fmt.Println("No next, you're on the last page")
+	} else {
+		fmt.Println(*cfg.next)
+	}
+	// DEBUG PRINT PREVIOUS
+	if cfg.previous == nil {
+		fmt.Println("No previous, you're on the first page")
+	} else {
+		fmt.Println(*cfg.previous)
+	}
+
+	for _, loc := range locations.Results {
+    fmt.Println(loc.Name)
+	}
+
 	return nil
 }
 
